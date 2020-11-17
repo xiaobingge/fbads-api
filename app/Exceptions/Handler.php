7d17ba43f9f9860ed38e35aca22298d08507aa3a
bingge <bingge@juanpi.com>
2020-11-17 15:22:28 +0800

    优化

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,6 +5,10 @@ namespace App\Exceptions;
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Auth\AuthenticationException;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+use Tymon\JWTAuth\Exceptions\TokenBlacklistedException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
 
 class Handler extends ExceptionHandler
 {
@@ -47,17 +51,30 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        // detect instance
+        if ($exception instanceof  UnauthorizedHttpException ) {
+            // detect previous instance
+            if ($exception->getPrevious() instanceof  TokenExpiredException ) {
+                return response()->json(['code'=>1024 ,'msg' => 'TOKEN_EXPIRED'], $exception->getStatusCode());
+            } else if ($exception->getPrevious() instanceof TokenInvalidException) {
+                return response()->json(['code'=>1024 ,'msg' => 'TOKEN_INVALID'], $exception->getStatusCode());
+            } else if ($exception->getPrevious() instanceof TokenBlacklistedException) {
+                return response()->json(['code'=>1024 ,'msg' => 'TOKEN_BLACKLISTED'], $exception->getStatusCode());
+            } else {
+                return response()->json(['code'=>1024 ,'msg' => "UNAUTHORIZED_REQUEST"], 401);
+            }
+        }
         return parent::render($request, $exception);
     }
 
 
-    protected function unauthenticated($request, AuthenticationException $exception) {
-        if ($request->expectsJson()) {
-            return response()->json(['error' => 'Unauthenticated.'], 401);
-        }
-        if(in_array('admin', $exception->guards())) {
-            return redirect()->guest('/admin/login');
-        }
-        return redirect()->guest('login');
-    }
+//    protected function unauthenticated($request, AuthenticationException $exception) {
+//        if ($request->expectsJson()) {
+//            return response()->json(['error' => 'Unauthenticated.'], 401);
+//        }
+//        if(in_array('admin', $exception->guards())) {
+//            return redirect()->guest('/admin/login');
+//        }
+//        return redirect()->guest('login');
+//    }
 }
