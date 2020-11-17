f8b5681fc63a8fdb9456fc112a7d2f0cff77b16d
meixi <meixi@juanpi.com>
2020-11-17 16:22:13 +0800

    添加跳转中转

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,16 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::post('login', 'ApiController@login');
+Route::post('register', 'ApiController@register');
+Route::get('click', 'ClickController@index');
+
+
+Route::group(['middleware' => 'auth.jwt'], function () {
+    Route::get('logout', 'ApiController@logout');
+    Route::get('user', 'ApiController@getAuthUser');
+
+});
+
