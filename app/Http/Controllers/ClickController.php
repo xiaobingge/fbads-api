ac34f3e5984620064b003ac6dbd6586fd67381cf
meixi <meixi@juanpi.com>
2020-11-25 12:41:24 +0800

    添加跳转

--- app/Http/Controllers/ClickController.php
+++ app/Http/Controllers/ClickController.php
@@ -28,6 +28,37 @@ class ClickController extends Controller
 			header("location:".$url);die;
 		}
 
+
+		if($_GET['image_url']) {
+			$imageUrl = $_GET['image_url'];
+			$imageUrl = stripos($imageUrl, 'https') === false ? 'https:'.$imageUrl : $imageUrl;
+
+			$ch = curl_init($imageUrl);
+			curl_setopt ($ch, CURLOPT_REFERER, 'https://www.facebook.com');
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($ch, CURLOPT_BINARYTRANSFER, 1);
+			curl_setopt($ch, CURLOPT_HEADER, 0);
+			curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+			curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE );
+			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE );
+			$data = curl_exec($ch);
+			$info = curl_getinfo($ch);
+			curl_close($ch);
+			$httpCode = intval($info['http_code']);
+			$httpSizeDownload= intval($info['size_download']);
+
+			if($httpCode!='200' || $httpSizeDownload<1){
+				return ['code'=>1000, 'msg'=>'获取成功', 'data'=>['image_info'=>'', 'width'=>0, 'height'=>0]];
+			}
+			$baseImageInfo = base64_encode($data);
+			list($width, $height) = getimagesize($imageUrl);
+
+			$sizeArr = getimagesize('data://image/jpeg;base64,'. $baseImageInfo);
+			$width = $sizeArr[0];
+			$height = $sizeArr[1];
+			return ['code'=>1000, 'msg'=>'获取成功', 'data'=>['image_info'=>$baseImageInfo, 'width'=>$width, 'height'=>$height]];
+		}
 	}
 
 
