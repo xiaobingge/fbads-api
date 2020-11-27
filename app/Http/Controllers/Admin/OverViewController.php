6ee7571b16b23e230c4c268603d8604096618436
bingge <bingge@juanpi.com>
2020-11-27 14:02:22 +0800

    code

--- app/Http/Controllers/Admin/OverViewController.php
+++ app/Http/Controllers/Admin/OverViewController.php
@@ -19,7 +19,7 @@ class OverViewController extends Controller{
         $obj = AdOverview::where('date','>=',$start)->where('date' , '<=' ,$end);
         if(!empty($accounts))
             $obj->where('account_id','=',$accounts);
-        $list =$obj->select('date',DB::raw('SUM(spend) as spend'),DB::raw('SUM(impression) as impression') , DB::raw('SUM(click) as click') , DB::raw('SUM(install) as install') ,
+        $list =$obj->select('date',DB::raw('SUM(spend) as spend'),DB::raw('SUM(impression) as impression') , DB::raw('SUM(inline_link_clicks) as inline_link_clicks') , DB::raw('SUM(click) as click') , DB::raw('SUM(install) as install') ,
                 DB::raw('SUM(landing_page_view) as landing_page_view'),DB::raw('SUM(add_cart) as add_cart'),DB::raw('SUM(purchase) as purchase'),DB::raw('SUM(purchase_value) as purchase_value'))
             ->groupBy('date')
             ->orderBy('date' , 'desc')
@@ -34,18 +34,19 @@ class OverViewController extends Controller{
             $data['add_cart'] +=$value['add_cart'];
             $data['purchase'] += $value['purchase'];
             $data['purchase_value'] += $value['purchase_value'];
-            $list[$key]['ctr'] = $value['impression'] > 0 ? round($value['click']*100/$value['impression'],2) : 0;
-            $list[$key]['cpc'] = $value['click'] > 0 ? round($value['spend']/$value['click'],2) : 0;
+            $data['inline_link_clicks'] += $value['inline_link_clicks'];
+            $list[$key]['ctr'] = $value['impression'] > 0 ? round($value['inline_link_clicks']*100/$value['impression'],2) : 0;
+            $list[$key]['cpc'] = $value['inline_link_clicks'] > 0 ? round($value['spend']/$value['inline_link_clicks'],2) : 0;
             $list[$key]['cpm'] = $value['impression'] > 0 ?  round($value['spend']*1000/$value['impression'],2) : 0;
             $list[$key]['cpa'] = $value['purchase'] > 0 ?  round($value['spend']/$value['purchase'],2) : 0;
             $list[$key]['roas'] =$value['purchase_value'] > 0 ? round($value['spend']/$value['purchase_value'],2) : 0;
         }
-        $data['ctr'] = $data['impression'] > 0 ? round($data['click']*100/$data['impression'],2) : 0 ;
-        $data['cpc'] = $data['click'] > 0 ?  round($data['spend']/$data['click'],2) : 0;
+        $data['ctr'] = $data['impression'] > 0 ? round($data['inline_link_clicks']*100/$data['impression'],2) : 0 ;
+        $data['cpc'] = $data['click'] > 0 ?  round($data['spend']/$data['inline_link_clicks'],2) : 0;
         $data['cpm'] = $data['impression'] > 0 ?  round($data['spend']*1000/$data['impression'],2) : 0;
         $data['cpa'] = $data['purchase'] > 0 ?  round($data['spend']/$data['purchase'],2) : 0;
         $data['roas'] =$data['purchase_value'] > 0 ?  round($data['spend']/$data['purchase_value'],2) : 0;
-        $accounts = AdAccount::where(['user_id'=>$request->input('user_id')])->get();
+        $accounts = AdAccount::get();
         return success(['list'=>$list,'total'=>$data,'accounts'=>$accounts]);
 
     }
